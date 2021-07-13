59e7d3daf9be9c596ad8b0ab567ddb0820f663d7
Tim <megatimtams@gmail.com>
2021-07-12 17:23:19 -0700

    Refined doCalc, Added html to show result

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
 function doCalc() {
     let num1 = document.getElementById("fnum").value;
     let num2 = document.getElementById("snum").value;
-    let result = 0;
-    let operator = Math.random * (4-1) + 1;
-    if (operator == 1 && ((num1+num2)%1 == 0)) {
-        result = num1+num2
+    let result = 0.1;
+    do {
+        let operator = Math.floor(Math.random() * (4 - 1 + 1) + 1);
+        if (operator == 1 && ((num1+num2)%1 == 0)) {
+            result = num1+num2
+        }
+        if (operator == 2 && ((num1-num2)%1 == 0)) {
+            result = num1-num2;
+        }
+        if (operator == 3 && ((num1/num2)%1 == 0)) {
+            result = num1/num2;
+        }
+        if (operator == 4 && ((num1*num2)%1 == 0)) {
+            result = num1*num2;
+        }
     }
-    if (operator == 2 && ((num1-num2)%1 == 0)) {
-        result = num1-num2;
-    }
-    return [operator, result];
+    while (result == 0.1)
+    //alert(result.toString() + " | " + operator.toString());
+    document.getElementById("result").innerHTML = "RESULT: " + result.toString();
 }
 
 //function checkWhole()
