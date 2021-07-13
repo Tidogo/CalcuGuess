8493e5d434a65d973dc2bd740ab2d71aac57d90a
Tim <megatimtams@gmail.com>
2021-07-13 15:06:13 -0700

    Added baseline commenting, finished functions and html.

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
+// Author: Timothy Goddard
+// Description: This is the Javascript file used in the CalcuGuess project. It contains the functions required to properly operate the guessing game.
+// Date: 07/13/2021
+let operator = 0;
 function doCalc() {
-    let num1 = document.getElementById("fnum").value;
-    let num2 = document.getElementById("snum").value;
+    let num1 = parseInt(document.getElementById("fnum").value);
+    let num2 = parseInt(document.getElementById("snum").value);
     let result = 0.1;
     do {
-        let operator = Math.floor(Math.random() * (4 - 1 + 1) + 1);
+        operator = Math.floor(Math.random() * (4 - 1 + 1) + 1);
         if (operator == 1 && ((num1+num2)%1 == 0)) {
-            result = num1+num2
+            result = num1+num2;
         }
         if (operator == 2 && ((num1-num2)%1 == 0)) {
             result = num1-num2;
@@ -22,4 +26,25 @@ function doCalc() {
     document.getElementById("result").innerHTML =  "RESULT: " + result;
 }
 
-//function checkWhole()
+function checkOp() {
+    let result = false;
+    let userGuess = document.querySelectorAll('input[name="op"]');
+    for (const guess in userGuess) {
+        if (guess.checked) {
+            if (guess.value == operator) {
+                result = true;
+                break;
+            }
+        }
+    }
+    if (result == 0) {
+        alert("You have guessed correct!");
+        document.getElementById("result").innerHTML = "RESULT: "
+        document.getElementById("fnum").innerHTML = "";
+        document.getElementById("lnum").innerhtml = "";
+    }
+    else {
+        alert("You have guessed incorrectly! Try again")
+    }
+    
+}
